18ace31240ed24e4a576a06569b4e0a908fff6e1
Dev-Nadson <nadson.pros@gmail.com>
2026-04-15 11:59:09 -0300

    feat: implement users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,26 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, UseGuards } from "@nestjs/common";
 import { UserService } from "./user.service";
+import type { CreateUserInput, CreateUserOutput, GetUserInput, GetUserOutput } from "src/typings/user.types";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 
-@Controller('user')
+@Controller('users')
 export class UserController {
     constructor(private readonly userService: UserService) { }
-}
 
+    @Post()
+    async createUser(@Body() input: CreateUserInput): Promise<CreateUserOutput> {
+        return await this.userService.createUser(input);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    async listUsers(): Promise<GetUserOutput[]> {
+        return await this.userService.listUsers();
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get(':id')
+    async getUser(@Param() input: GetUserInput): Promise<GetUserOutput> {
+        return await this.userService.getUser(input);
+    }
+}
