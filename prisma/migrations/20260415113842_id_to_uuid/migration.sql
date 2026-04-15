a24bdb7bfa888e7c94fb589863d8377ba2435476
Dev-Nadson <nadson.pros@gmail.com>
2026-04-15 12:00:42 -0300

    refac: add if to CUID

---
+++ prisma/migrations/20260415113842_id_to_uuid/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - The primary key for the `users` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- AlterTable
+ALTER TABLE "users" DROP CONSTRAINT "users_pkey",
+ADD COLUMN     "deleted_at" TIMESTAMP(3),
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "users_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "users_id_seq";
