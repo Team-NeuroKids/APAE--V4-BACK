bdf24e1ad687a6ff4deb0a64696f45d712ac67fc
Dev-Nadson <nadson.pros@gmail.com>
2026-04-21 03:00:43 -0300

    fix: reorder CORS and global validation pipe setup in bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
 
+  // todo: habilitar apenas a url do front
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
