4ee8e791587eb247843b9c78830488568bcd618e
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2026-05-05 01:34:29 -0300

    feat: create CommonModule and refactor UtilsService to use proper class naming and decorators

--- src/common/utils.service.ts
+++ src/common/utils.service.ts
@@ -1,19 +1,19 @@
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { hash, compare } from 'bcryptjs';
 
-export class utilsService {
-  constructor(private configService: ConfigService) { }
+@Injectable()
+export class UtilsService {
+  constructor(private configService: ConfigService) {}
 
   async hash_password(password: string): Promise<string> {
-    return await hash(password, this.configService.getOrThrow<number>('BCRYPT_ROUNDS'));
+    return await hash(
+      password,
+      this.configService.getOrThrow<number>('BCRYPT_ROUNDS'),
+    );
   }
 
-  async compare_password(
-    password: string,
-    hash: string,
-  ): Promise<boolean> {
+  async compare_password(password: string, hash: string): Promise<boolean> {
     return await compare(password, hash);
   }
-
 }
-
