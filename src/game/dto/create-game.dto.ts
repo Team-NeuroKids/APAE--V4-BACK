53085022de0f2d51c697a073b77afc9a9e7b1815
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2026-05-08 00:59:25 -0300

    feat: validate game DTOs with class-validator

--- src/game/dto/create-game.dto.ts
+++ src/game/dto/create-game.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 export class CreateGameDto {
+  @IsString()
+  @IsNotEmpty()
   title!: string;
+
+  @IsString()
+  @IsOptional()
   description?: string;
+
+  @IsString()
+  @IsOptional()
   thumbnail?: string;
 }
